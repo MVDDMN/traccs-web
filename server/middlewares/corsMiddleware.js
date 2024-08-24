c3eb745aa9565fc51029f946ea0bd32d6b8579e6
MVDDMN <delacruzkp@students.national-u.edu.ph>
2024-08-25 00:04:54 +0800

    August 24 - backend corsMiddleware changes

--- server/middlewares/corsMiddleware.js
+++ server/middlewares/corsMiddleware.js
@@ -2,11 +2,19 @@ const cors = require("cors");
 require('dotenv').config();
 
 const allowedOrigins = process.env.NODE_ENV === 'production'
-    ? [process.env.PROD_BASE_URL]
-    : [process.env.DEV_BASE_URL, process.env.DEV_ALT_URL];
+    ? [process.env.PROD_BASE_URL].filter(Boolean)
+    : [process.env.DEV_BASE_URL, process.env.DEV_ALT_URL].filter(Boolean);
 
 const corsMiddleware = cors({
-    origin: allowedOrigins,
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        } else {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+    },
     credentials: true,
 });
 
