6936d10f97960e649b2c44916994b38710a4572a
MVDDMN <delacruzkp@students.national-u.edu.ph>
2024-08-24 23:26:39 +0800

    August 24 - bcryptjs changed

--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const adminModel = require('../models/admin');
 const usersModel = require('../models/users');
 const router = express.Router();
