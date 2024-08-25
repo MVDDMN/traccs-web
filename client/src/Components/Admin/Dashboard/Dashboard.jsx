7ae42dc379c4ed34b3b85973918d849978ea2f4c
MVDDMN <delacruzkp@students.national-u.edu.ph>
2024-08-25 18:58:55 +0800

    August 25: Frontend - Dashboard Fetch Interval Fix

--- client/src/Components/Admin/Dashboard/Dashboard.jsx
+++ client/src/Components/Admin/Dashboard/Dashboard.jsx
@@ -113,7 +113,7 @@ const Dashboard = () => {
         };
 
         fetchReports();
-        const interval = setInterval(fetchReports, 1000);
+        const interval = setInterval(fetchReports, 5000);
 
         return () => clearInterval(interval);
     }, []);
