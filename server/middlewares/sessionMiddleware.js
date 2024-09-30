725d53552933e4a3fc1ab6873240105f5b67e86b
MVDDMN <delacruzkp@students.national-u.edu.ph>
2024-09-30 20:54:40 +0800

    September 30: Session Key Update

--- server/middlewares/sessionMiddleware.js
+++ server/middlewares/sessionMiddleware.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const session = require("express-session");
 
 const sessionMiddleware = session({
-    secret: 'your-secret-key',
+    secret: process.env.SESSION_SECRET, // Use an environment variable for the secret
     resave: false,
     saveUninitialized: false,
     rolling: true,
     cookie: {
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 1800000,
+        secure: process.env.NODE_ENV === 'production', // Set cookie security based on environment
+        maxAge: 1800000, // 30 minutes
         sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
         httpOnly: true,
     },
