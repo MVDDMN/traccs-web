8e351010c9b082274a0ffabd590835c1627c5299
MVDDMN <delacruzkp@students.national-u.edu.ph>
2024-10-03 13:17:31 +0800

    October 3 : Frontend Security Changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,10 +10,9 @@ export default defineConfig({
   },
   server: {
     headers: {
-      'Strict-Transport-Security': 'max-age=63072000; includeSubDomains; preload',
-      // Added subdomains (a, b, c) of CartoDB basemap URL to img-src
+      'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
       'Content-Security-Policy': "default-src 'self'; img-src 'self' data: https://a.basemaps.cartocdn.com https://b.basemaps.cartocdn.com https://c.basemaps.cartocdn.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; script-src 'self' 'unsafe-inline'; connect-src 'self' http://localhost:3001; frame-ancestors 'none';",
-      'X-Frame-Options': 'DENY',
+      'X-Frame-Options': 'SAMEORIGIN',
       'Referrer-Policy': 'no-referrer',
       'Permissions-Policy': 'geolocation=(self)',
       'X-Content-Type-Options': 'nosniff',
